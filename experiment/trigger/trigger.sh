4263747883953532e83f66a7c5e04eb1668cf827
Hsiang-ling Tai <nicole1999224@gmail.com>
2024-08-25 11:34:10 +0000

    trigger script

--- experiment/trigger/trigger.sh
+++ experiment/trigger/trigger.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 USER_START=1
-USER_END=11
+USER_END=13
 
 # centralized, federated, decentralized
-ARCH="decentralized"
-CONFIG_LOCUST="config/locust/${ARCH}/cpu.json"
+ARCH="federated"
+# centralized, distributed
+ARRIVAL="distributed"
+WORKLOAD="resize-image"
+CONFIG_LOCUST="config/locust/${ARCH}/cpu-${ARRIVAL}.json"
 CONFIG_PROM="config/prom/${ARCH}/config_prom.json"
 ANSIBLE_PATH="/home/thl/edge-cloud-continuum-openfaas/deployment/ansible"
 
@@ -13,25 +16,111 @@ function log() {
     timestamp=`date "+%Y-%m-%d %H:%M:%S"`
     echo "[${USER}][${timestamp}][${1}]: ${2}"
 }
+function scale_to_one() {
+    user=$1
+    password=$2
+    host=$3
+    log "INFO" "Scale on ${host}"
+    status_code=$(curl --location --silent --output /dev/null --write-out "%{http_code}" \
+        "http://${user}:${password}@${host}/system/scale-function/${WORKLOAD}" \
+        --header 'Content-Type: application/json' \
+        --data '{
+            "serviceName": "'"${WORKLOAD}"'",
+            "namespace": "openfaas-fn",
+            "replicas": 1
+        }'
+    )
+    if [ "$status_code" -eq 202 ]; then
+        log "INFO" "Scale successfully. Wait for 3 seconds ..."
+        sleep 3
+        log "INFO" "Done"
+        return 0
+    else
+        return 1
+    fi
+}
+function initialize_federated () {
+    dirty=0
+
+    scale_to_one "admin" "KNuOALKrKsSFtkOXucIX" "145.100.135.86:31112"
+    ret_val=$? 
+    if [ ${ret_val} -eq 0 ]; then
+        dirty=1
+    fi
+    
+
+    scale_to_one "admin" "IXuJycmvzEQWvGuQJnQr" "192.168.100.103:31112"
+    ret_val=$? 
+    if [ ${ret_val} -eq 0 ]; then
+        dirty=1
+    fi
+
+    scale_to_one "admin" "fgHJzWYgeoOHkzeEBzRH" "192.168.100.105:31112"
+    ret_val=$? 
+    if [ ${ret_val} -eq 0 ]; then
+        dirty=1
+    fi
+
+    if [ ${dirty} -eq 1 ]; then
+        log "INFO" "Scale on somewhere. Wait 60 seconds..."
+        sleep 60
+        log "INFO" "Done"
+    fi
+
+}
+# function initialize_federated () {
+#     status_code=$(curl --location --silent --output /dev/null --write-out "%{http_code}" \
+#         "http://admin:KNuOALKrKsSFtkOXucIX@145.100.135.86:31112/system/scale-function/${WORKLOAD}" \
+#         --header 'Content-Type: application/json' \
+#         --data '{
+#             "serviceName": "'"${WORKLOAD}"'",
+#             "namespace": "openfaas-fn",
+#             "replicas": 1
+#         }')
+#     sleep 3
+
+#     curl --location "http://admin:IXuJycmvzEQWvGuQJnQr@192.168.100.103:31112/system/scale-function/${WORKLOAD}" \
+#         --header 'Content-Type: application/json' \
+#         --data '{
+#             "serviceName": "'"${WORKLOAD}"'",
+#             "namespace": "openfaas-fn",
+#             "replicas": 1
+#         }'
+#     sleep 3
+
+#     curl --location "http://admin:fgHJzWYgeoOHkzeEBzRH@192.168.100.105:31112/system/scale-function/${WORKLOAD}" \
+#         --header 'Content-Type: application/json' \
+#         --data '{
+#             "serviceName": "'"${WORKLOAD}"'",
+#             "namespace": "openfaas-fn",
+#             "replicas": 1
+#         }'
+    
+#     sleep 3
+
+#     sleep 60
+
+# }
 
 main() {
 
 
     for user_cnt in $(seq ${USER_START} ${USER_END}); do
-        # deploy on cloud and clean all edge
-        # ansible-playbook -i ${ANSIBLE_PATH}/inventory/federated.ini ${ANSIBLE_PATH}/experiment.yaml
-        
+
         # sleep 30
         # trigger the trigger point once to prevent cold start, and also to gave a record of fastest trigger
-        sleep 3
-        curl http://192.168.100.105:8080/function/floating-point-operation-sine
+        # sleep 3
+        # curl http://192.168.100.105:31112/function/resize-image/
         # curl http://192.168.100.102:8080/function/sorter
         # curl http://192.168.100.103:8080/function/sorter
         # curl http://192.168.100.111:8080/function/sorter
         # curl http://145.100.135.86:31112/function/sorter
-        sleep 3
+        # sleep 3
         #
-        python3 main.py -l ${CONFIG_LOCUST} -p ${CONFIG_PROM} -c case${user_cnt}
+        initialize_federated
+
+        python3 main.py -c case${user_cnt} -l ${CONFIG_LOCUST} -p ${CONFIG_PROM}
+        
         mkdir -p result/${ARCH} 2> /dev/null
         ls *.csv 2>/dev/null && mv *.csv result/${ARCH}
     done
